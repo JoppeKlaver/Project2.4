416c649c932c14f7aec7b3a5bcb8d9a52f086bed
Nick Flokstra <nick.flokstra@hotmail.nl>
2019-05-22 14:15:26 +0200

    Top 5 added

--- Memory/src/app/top5/top5.component.ts
+++ Memory/src/app/top5/top5.component.ts
@@ -1,11 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Player } from './player'
+
 @Component({
   selector: 'app-top5',
   templateUrl: './top5.component.html',
   styleUrls: ['./top5.component.css']
 })
 export class Top5Component implements OnInit {
+
+  const TOP5: Player[] =  [{
+        name: "Barack Obama",
+        time: 200
+    },
+    {
+        name: "Bernie Sanders",
+        time: 300
+    },
+    {
+        name: "Hillary Clinton",
+        time: 400
+    },
+    {
+        name: "Jeb Bush",
+        time: 500
+    },
+    {
+        name: "Donald Trump",
+        time: 600
+    }
+]
 
   constructor() { }
 
