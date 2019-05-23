8d0917801f9d563f7b86c4c5d60e9a0aa93da746
Kevin Jersak <KevinJersak@hotmail.com>
2019-05-23 14:35:48 +0200

    colour component toegevoegd

--- Memory/src/app/colour/colour.component.ts
+++ Memory/src/app/colour/colour.component.ts
@@ -6,10 +6,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./colour.component.css']
 })
 export class ColourComponent implements OnInit {
+  inactiveColour = "#f73333";
+  activeColour = "#3444bc";
+  foundColour = "#0cff00";
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  changeInactiveColour(event: any){
+    this.inactiveColour = event.target.value;
+  }
+
+  changeActiveColour(event: any){
+    this.activeColour = event.target.value;
+  }
+
+  changeFoundColour(event: any){
+    this.foundColour = event.target.value;
   }
 
 }
